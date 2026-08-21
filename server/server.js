eda96ecf9f5163183c8d4650e8ab1a3e20ea9981
Rachit Gangwar <156359363+Rachit-0001@users.noreply.github.com>
2026-08-21 16:47:42 +0530

    fix commit

--- server/server.js
+++ server/server.js
@@ -22,7 +22,11 @@ const app = express();
 
 app.use(cors({
     origin: process.env.CLIENT_URL
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+    allowedHeaders: ["Content-Type", "Authorization"]
 }));
+
+
 app.use(express.json());
 
 app.get("/", (req, res) => {
